830e62d887e57ac0f38e68d41ae38d6b7a57441b
A1esandr <a1exswan001@gmail.com>
2022-08-05 21:28:03 +0500

    alt quotes

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -7,3 +7,8 @@
 puts 'Text'
 puts 'Simple output'
 puts "Simple output\nSecond output"
+
+name = 'Charlie Brown'
+ 
+print %Q!Say "Hello," #{name}.\n!
+print %Q/What is "4 plus 5"? Answer: #{4+5}\n/
