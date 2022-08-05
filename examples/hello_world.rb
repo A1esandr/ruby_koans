2ad50583c8cbc78249dc3a5296312104ad2b1977
A1esandr <a1exswan001@gmail.com>
2022-08-05 20:45:18 +0500

    cli use

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -1,3 +1,9 @@
+#!/usr/bin/ruby
+
+# not forget to chmod +x hello_world.rb
+# for usage as executable 
+# like ./hello_world.rb
+
 puts 'Text'
 puts 'Simple output'
 puts "Simple output\nSecond output"
