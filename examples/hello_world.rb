64c86245289419519ea1f8ccbef1683e7f8585af
A1esandr <a1exswan001@gmail.com>
2022-08-05 21:30:56 +0500

    added ex

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -12,3 +12,12 @@ name = 'Charlie Brown'
  
 print %Q!Say "Hello," #{name}.\n!
 print %Q/What is "4 plus 5"? Answer: #{4+5}\n/
+
+name = 'Charlie Brown'
+
+puts <<QUIZ
+Student: #{name}
+ 
+1.\tQuestion: What is 4+5?
+\tAnswer: The sum of 4 and 5 is #{4+5}
+QUIZ
